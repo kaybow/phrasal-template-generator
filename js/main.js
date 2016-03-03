a390394d14f9e1f76030794541d3154ae3048d9c
kaybow <k.bowmaster@hotmail.com>
2016-03-03 13:39:54 -0500

    Added in plus signs and document.write to sentences

--- js/main.js
+++ js/main.js
@@ -13,14 +13,14 @@ var newVerb = prompt('A past tense verb (ran, burped, etc.'))
 var newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:')
 
 if (newStory == 'a') {
-  After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+  document.write('After snacking on ' + adjective + 'treats, the ' + colour + 'bellied ' + creature + ' ' + verbed ' for hours.');
 }
 
 if (newStory == 'b') {
-  Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+  document.write('Ameilia ' + verbed + 'through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 }
 
 if (newStory == 'c') {
-  Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+  document.write('Jackson chose his ' + adjective, + ' ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
 }
 
