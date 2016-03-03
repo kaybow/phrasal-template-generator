80faadeac0465e993850200ac6110bb500e5c1a3
kaybow <k.bowmaster@hotmail.com>
2016-03-03 13:32:49 -0500

    Working on if variables

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
+var newColour;
+var newCreature;
+var newAdj;
+var newStory;
+
 	alert('Let’s make a story together!');
 }
 
-var newColour = prompt('A colour (orange, indigo, etc.')
+var newColour = prompt('A colour (orange, indigo, etc.'))
+var newCreature = prompt('A creature (dragon, moth, etc.'))
+var newAdj = prompt('An adjective (beautiful, super, etc.'))
+var newVerb = prompt('A past tense verb (ran, burped, etc.'))
+var newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:')
 
-
-var newCreature = prompt('A creature (dragon, moth, etc.')
-
-
-var newAdj = prompt('An adjective (beautiful, super, etc.')
-
-
-var newVerb = prompt('A past tense verb (ran, burped, etc.')
-
-
-var newStory = prompt('Which story would you like?
-
-a) Snacking
-b) Escaping
-c) Winning
-
-Enter the letter below:')
-
-if (newStory = 'Snacking') {
+if (newStory == 'a') {
   After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
 }
 
-if (newStory = 'Escaping') {
+if (newStory == 'b') {
   Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
 }
 
-if (newStory = 'Winning') {
+if (newStory == 'c') {
   Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
 }
 
