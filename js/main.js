0f306be2fbc86f806134280cf8aedccf15f36fb6
kaybow <k.bowmaster@hotmail.com>
2016-03-03 09:57:31 -0500

    Working on if-statements

--- js/main.js
+++ js/main.js
@@ -5,20 +5,32 @@
 var newColour = prompt('A colour (orange, indigo, etc.')
 
 
-var newCreature = ('A creature (dragon, moth, etc.')
+var newCreature = prompt('A creature (dragon, moth, etc.')
 
 
-var newAdj = ('An adjective (beautiful, super, etc.')
+var newAdj = prompt('An adjective (beautiful, super, etc.')
 
 
-var newVerb = ('A past tense verb (ran, burped, etc.')
+var newVerb = prompt('A past tense verb (ran, burped, etc.')
 
 
-var newStory = ('Which story would you like?
+var newStory = prompt('Which story would you like?
 
 a) Snacking
 b) Escaping
 c) Winning
 
 Enter the letter below:')
+
+if (newStory = 'Snacking') {
+
+}
+
+if (newStory = 'Escaping') {
+
+}
+
+if (newStory = 'Winning') {
+
+}
 
