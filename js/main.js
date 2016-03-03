bb8926edb21d6eb2ff0af032c23f9e2478d39529
kaybow <k.bowmaster@hotmail.com>
2016-03-03 09:55:38 -0500

    Added prompts

---
+++ js/main.js
@@ -0,0 +1,24 @@
+	alert('Let’s make a story together!');
+}
+
+
+var newColour = prompt('A colour (orange, indigo, etc.')
+
+
+var newCreature = ('A creature (dragon, moth, etc.')
+
+
+var newAdj = ('An adjective (beautiful, super, etc.')
+
+
+var newVerb = ('A past tense verb (ran, burped, etc.')
+
+
+var newStory = ('Which story would you like?
+
+a) Snacking
+b) Escaping
+c) Winning
+
+Enter the letter below:')
+
