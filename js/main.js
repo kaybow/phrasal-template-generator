6485bc01521c172298a55ff0ea8e88c7a933e8b4
kaybow <k.bowmaster@hotmail.com>
2016-03-04 14:52:34 -0500

    Fixed market issues

--- js/main.js
+++ js/main.js
@@ -5,21 +5,19 @@ var newStory;
 
 alert('Let’s make a story together!');
 
-newColour = prompt('A colour (orange, indigo, etc.'))
-newCreature = prompt('A creature (dragon, moth, etc.'))
-newAdj = prompt('An adjective (beautiful, super, etc.'))
-newVerb = prompt('A past tense verb (ran, burped, etc.'))
-newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:'))
+newColour = prompt('A colour (orange, indigo, etc.)');
+newCreature = prompt('A creature (dragon, moth, etc.)');
+newAdj = prompt('An adjective (beautiful, super, etc.)');
+newVerb = prompt('A past tense verb (ran, burped, etc.)');
+newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:');
 
 if (newStory == 'a') {
-  document.write('After snacking on ' + newAdj + 'treats, the ' + newColour + 'bellied ' + newCreature + ' ' + newVerb ' for hours.');
-}
+  document.write('After snacking on ' + newAdj + 'treats, the ' + newColour + 'bellied ' + newCreature + ' ' + newVerb + ' for hours.');
 
-if (newStory == 'b') {
-  document.write('Ameilia ' + newVerb + 'through the ' + newAdj + ' ' + newColour + ' nebula escaping the space ' + newCreature + '.');
-}
+} if (newStory == 'b') {
+  document.write('Ameilia ' + newVerb + ' through the ' + newAdj + ' ' + newColour + ' nebula escaping the space ' + newCreature + '.');
 
-if (newStory == 'c') {
-  document.write('Jackson chose his ' + newAdj, + ' ' + newColour + ' ' + newCreature + ' card and ' + newVerb  + ' it to the table knowing he won.');
+} if (newStory == 'c') {
+  document.write('Jackson chose his ' + newAdj + ', ' + newColour + ' ' + newCreature + ' card and ' + newVerb  + ' it to the table knowing he won.');
 }
 
