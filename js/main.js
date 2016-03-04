dac078d46e4b519f2b7e2ab39a87635f86e24592
kaybow <k.bowmaster@hotmail.com>
2016-03-04 14:46:45 -0500

    Fixed naming issues

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var newCreature;
 var newAdj;
 var newStory;
 
-	alert('Let’s make a story together!');
+alert('Let’s make a story together!');
 
 newColour = prompt('A colour (orange, indigo, etc.'))
 newCreature = prompt('A creature (dragon, moth, etc.'))
@@ -12,14 +12,14 @@ newVerb = prompt('A past tense verb (ran, burped, etc.'))
 newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:'))
 
 if (newStory == 'a') {
-  document.write('After snacking on ' + adjective + 'treats, the ' + colour + 'bellied ' + creature + ' ' + verbed ' for hours.');
+  document.write('After snacking on ' + newAdj + 'treats, the ' + newColour + 'bellied ' + newCreature + ' ' + newVerb ' for hours.');
 }
 
 if (newStory == 'b') {
-  document.write('Ameilia ' + verbed + 'through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+  document.write('Ameilia ' + newVerb + 'through the ' + newAdj + ' ' + newColour + ' nebula escaping the space ' + newCreature + '.');
 }
 
 if (newStory == 'c') {
-  document.write('Jackson chose his ' + adjective, + ' ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+  document.write('Jackson chose his ' + newAdj, + ' ' + newColour + ' ' + newCreature + ' card and ' + newVerb  + ' it to the table knowing he won.');
 }
 
