157cc3008f2c842390ad8f80be44f4cc17ddb2c6
kaybow <k.bowmaster@hotmail.com>
2016-03-04 14:28:38 -0500

    Added extra closing bracket

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ newColour = prompt('A colour (orange, indigo, etc.'))
 newCreature = prompt('A creature (dragon, moth, etc.'))
 newAdj = prompt('An adjective (beautiful, super, etc.'))
 newVerb = prompt('A past tense verb (ran, burped, etc.'))
-newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:')
+newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning Enter the letter below:'))
 
 if (newStory == 'a') {
   document.write('After snacking on ' + adjective + 'treats, the ' + colour + 'bellied ' + creature + ' ' + verbed ' for hours.');
